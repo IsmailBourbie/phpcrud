63afe8c4205ba50bbae077c7f7e92bf8b5f6714f
Ismail Bourbie <bourbieismail@gmail.com>
2019-10-20 15:26:13 +0100

    add basic router

--- core/Router.php
+++ core/Router.php
@@ -1,5 +1,29 @@
 <?php
 
 Class Router {
+
+
+    // Routes Table 
+    public $routes = [];
+
+    // Save params from matched route
+    public $params = [];
+
+    // add route to routes table
+    public function add($route, $params)
+    {
+        $this->routes[$route] = $params;
+    }
+
+    // check if the requested url exists in our routes table
+    public function match($url) {
+        foreach($this->routes as $route => $params) {
+            if($url == $route) {
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }
     
 }
--- public/index.php
+++ public/index.php
@@ -1,4 +1,22 @@
 <?php
 
+require "../core/Router.php";
 
-echo "PHP MySQL CRUD Application";
+$router = new Router();
+
+
+$router->add("", ['controller' => 'Home', 'action' => 'index' ]);
+$router->add("posts", ['controller' => 'Posts', 'action' => 'index' ]);
+$router->add("posts/new", ['controller' => 'Posts', 'action' => 'new' ]);
+
+
+// get the url using query_string
+$url = $_SERVER['QUERY_STRING'];
+
+if($router->match($url)) {
+    var_dump($router->params);
+} else {
+    die("url not found");
+}
+
+// if($router->match())
