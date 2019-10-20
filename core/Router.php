3d17ed24f4e27ea17ceda93b6982a2a2e64747d6
Ismail Bourbie <bourbieismail@gmail.com>
2019-10-20 16:48:30 +0100

    add a complex system routing

--- core/Router.php
+++ core/Router.php
@@ -4,21 +4,46 @@ Class Router {
 
 
     // Routes Table 
-    public $routes = [];
+    public $routes = [
+        "GET" => [],
+        "POST" => [],
+        "PUT" => [],
+        "DELETE" => []
+    ];
 
     // Save params from matched route
     public $params = [];
 
-    // add route to routes table
-    public function add($route, $params)
-    {
-        $this->routes[$route] = $params;
-    }
+    // convert all string routes to regular expression
+        public function convertToRegX($route)
+        {
+            // escape / signe
+            $route = preg_replace('/\//','\\/', $route);
+
+            // convert custom variables e.g {id:\d+}
+            $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+
+            $route = "/^" . $route . "$/i";
+            return $route;
+        }
+
+        // add route to get table
+        public function get($route, $params = [] )
+        {
+            $route = $this->convertToRegX($route);
+            $this->routes["GET"][$route] = $params;
+        }
+        
 
     // check if the requested url exists in our routes table
-    public function match($url) {
-        foreach($this->routes as $route => $params) {
-            if($url == $route) {
+    public function match($url, $request) {
+        foreach($this->routes[$request] as $route => $params) {
+            if(preg_match($route, $url, $matches)) {
+                foreach($matches as $key => $param) {
+                    if(is_string($key)) {
+                        $params[$key] = $param;
+                    }
+                }
                 $this->params = $params;
                 return true;
             }
--- public/index.php
+++ public/index.php
@@ -5,15 +5,19 @@ require "../core/Router.php";
 $router = new Router();
 
 
-$router->add("", ['controller' => 'Home', 'action' => 'index' ]);
-$router->add("posts", ['controller' => 'Posts', 'action' => 'index' ]);
-$router->add("posts/new", ['controller' => 'Posts', 'action' => 'new' ]);
+// $router->add("{hello}/{world}", ['controller' => 'Home', 'action' => 'index' ]);
+$router->get("posts", ['controller' => 'Posts', 'action' => 'index' ]);
+$router->get("posts/new", ['controller' => 'Posts', 'action' => 'new' ]);
+$router->get("posts/{id:\d+}/new", ['controller' => 'Posts', 'action' => 'new' ]);
 
 
 // get the url using query_string
 $url = $_SERVER['QUERY_STRING'];
 
-if($router->match($url)) {
+
+var_dump($router->routes);
+
+if($router->match($url, 'GET')) {
     var_dump($router->params);
 } else {
     die("url not found");
