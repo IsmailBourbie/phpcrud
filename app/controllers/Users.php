e3a652fb923695351b1ca4d1e6e96850a8afe90c
Ismail Bourbie <bourbieismail@gmail.com>
2019-10-21 16:21:20 +0100

    add filter actions

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -4,8 +4,12 @@ namespace App\Controllers;
 use Core\Controller;
 
 class Users extends Controller {
-    public function add() {
-        echo "add user Controller";
+    public function addAction() {
+        echo "add user Controller<br>";
         echo 'id = ' . $this->route_params['id'];
+    }
+
+    protected function after(){
+        echo "<br> this is from after function";
     }
 }
--- core/Controller.php
+++ core/Controller.php
@@ -8,5 +8,33 @@ abstract Class Controller {
     {
         $this->route_params = $params;
     }
+
+
+    public function __call($name, $args)
+    {
+        $method = $name . "Action";
+        if(method_exists($this, $method)) {
+            if($this->before() !== false) {
+                call_user_func([$this, $method], $args);
+                $this->after();
+            }
+        } else {
+            die('method: ' . $action.' not exists in controller ' . $controller_object);
+        }
+    }
+
+
+    // this func must return true te call the action
+    // this func must overrated
+    protected function before() {
+        // Some code
+        return true;
+    }
+
+
+    // this func called after the func actio done
+    protected function after() {
+        // Some code
+    }
     
 }
