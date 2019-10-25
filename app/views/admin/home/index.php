d59d16354606c32d84b715fdca97c4dfcdf54862
Ismail Bourbie <bourbieismail@gmail.com>
2019-10-25 16:09:33 +0100

    design the navbar and index page

--- app/views/admin/home/index.php
+++ app/views/admin/home/index.php
@@ -4,8 +4,19 @@
 <?php require_once(App\Config::ROOT('APP') . "views/includes/navbar.php") ?>
 
 
-    <h1>Welcome to admin dashboard</h1>
+    <div class="dashboard container text-center">
+        <h1>PHP CRUD</h1>
 
+        <p class="lead">This is simple application of PHP CRUD Using MVC Architectur</p>
+
+        <div class="row text-center">
+            <div class="col-lg-4"></div>
+            <div class="col-lg-1"><a href="#">Github</a></div>
+            <div class="col-lg-1"><a href="#">Linkedin</a></div>
+            <div class="col-lg-1"><a href="#">Facebook</a></div>
+            <div class="col-lg-1"><a href="#">Instagram</a></div>
+        </div>
+    </div>
 
 
 <?php require_once(App\Config::ROOT('APP') . "views/includes/footer.php") ?>
--- public/routes.php
+++ public/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-$router->get("dashboard",['controller' => 'home', 'action' => 'index', 'namespace' => 'Admin']);
+$router->get("admin/dashboard",['controller' => 'home', 'action' => 'index', 'namespace' => 'Admin']);
 $router->get("users/add", ['controller' => 'users', 'action' => 'add', 'namespace' => 'Admin']);
 $router->get("users/{user:([a-z ]+)}/add", ['controller' => 'users', 'action' => 'add', 'namespace' => 'Admin']);
 $router->get("users/{id:\d+}/add", ['controller' => 'users', 'action' => 'add' ]);
